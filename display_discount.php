2804770ff49db0158feb814e8dea8eb9f36735ff
jkh1789 <kj59@njit.edu>
2017-02-06 17:46:24 -0500

    original

--- display_discount.php
+++ display_discount.php
@@ -1,8 +1,9 @@
 <?php
     // get the data from the form
-    $product_description = $_POST['Car'];
-    $list_price = $_POST['$50000'];
-    $discount_percent = $_POST['20%'];
+    $product_description = $_POST['product_description'];
+    $list_price = $_POST['list_price'];
+    $discount_percent = $_POST['discount_percent'];
+    
     
     // calculate the discount
     $discount = $list_price * $discount_percent * .01;
@@ -16,7 +17,11 @@
     
     // escape the unformatted input
     $product_description_escaped = htmlspecialchars($product_description);
+    
+
 ?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -28,13 +33,13 @@
         <h1>Product Discount Calculator</h1>
 
         <label>Product Description:</label>
-        <span><?php echo $product_description_escaped; ?></span><br>
+        <span><?php echo $pproduct_description_escaped; ?></span><br>
 
         <label>List Price:</label>
         <span><?php echo $list_price_formatted; ?></span><br>
 
         <label>Standard Discount:</label>
-        <span><?php echo $discount_percent_formatted; ?></span><br>
+        <span><?php echo $discount_percent_formateed; ?></span><br>
 
         <label>Discount Amount:</label>
         <span><?php echo $discount_formatted; ?></span><br>
